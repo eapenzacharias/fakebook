4b30492c3e98204a6a23cfa1d654b50139bce145
lilleonz <lilleonz@gmail.com>
2019-08-22 18:58:59 -0400

    one more down stickler lane :)

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
+# frozen_string_literal: true
+
+#  Routes for our fakebook app
 Rails.application.routes.draw do
   root to: 'home#signin'
   devise_for :users, controllers: { sessions: 'users/sessions' }
