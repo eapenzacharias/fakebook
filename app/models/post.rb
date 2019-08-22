333c7c10063bc5271dbdb4a7011d8060fad91fc3
lilleonz <lilleonz@gmail.com>
2019-08-22 16:52:24 -0400

    updated per tse request

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,7 @@
 class Post < ApplicationRecord
-    validates :user, presence :true
-    validates :content, presence :true
 
-    belongs_to :user, class_name: :User, foreign_key: :user_id
+  validates :user, presence: true
+  validates :content, presence: true
+
+  belongs_to :user, class_name: :User, foreign_key: :user_id
 end
--- fakebook/config/routes.rb
+++ fakebook/config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
   root to: 'home#signin'
+  
   devise_for :users, controllers: { sessions: 'users/sessions' }
 
 end
