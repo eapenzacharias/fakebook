0f8a82a0354ff3eb100703abc5f46033b49c5012
eapenzacharias <eapenzac@gmail.com>
2019-09-03 20:01:08 +0200

    Solved Missing name parameter

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -44,7 +44,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_sign_up_params
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :birthday, :attribute])
   end
 
   # If you have extra params to permit, append them to the sanitizer.
