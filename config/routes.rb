64d9abaf256df43dbd15e3e751e4af857983413c
lilleonz <lilleonz@gmail.com>
2019-09-09 15:05:26 -0400

    made routes.rb blank will reupload it then. stickler keeps reading and old copy of it.

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,3 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: { confirmations: 'users/confirmations', passwords: 'users/passwords', registrations: 'users/registrations', sessions: 'users/sessions', unlocks: 'users/unlocks', omniauth_callbacks: 'user/omniauth_callbacks' }
-  resources :users, only: [:show]
-  resources :posts, only: [:new, :create, :show, :destroy, :edit, :update]
-  devise_scope :user do
-    authenticated :user do
-      root 'feed#index', as: :authenticated_root
-    end
-    unauthenticated do
-      root 'users/sessions#new', as: :unauthenticated_root
-    end
-  end
-  get '/posts', to: 'feed#index'
-  get '/feed', to: 'feed#index'
+  
 end
