77c8d64b0db2a2abf20b906f64c6dddaed7b67c8
eapenzacharias <eapenzac@gmail.com>
2019-09-09 20:38:39 +0200

    Added testes for email presence and vaild email

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -23,6 +23,28 @@ RSpec.describe User, type: :model do
     expect(user.errors[:l_name]).to include('can\'t be blank')
   end
 
+  it 'is invalid without an email address' do
+    user = User.new(
+      name: 'Jane',
+      l_name: 'Tester',
+      email: '',
+      password: 'tester'
+    )
+    user.valid?
+    expect(user.errors[:email]).to include('can\'t be blank')
+  end
+  
+  it 'is invalid without a vaild email address' do
+    user = User.new(
+      name: 'Jane',
+      l_name: 'Tester',
+      email: 'tester',
+      password: 'tester'
+    )
+    user.valid?
+    expect(user.errors[:email]).to include('is invalid')
+  end
+
   it 'is invalid with a duplicate email address' do
     User.create(
       name: 'Joe',
@@ -39,4 +61,5 @@ RSpec.describe User, type: :model do
     user.valid?
     expect(user.errors[:email]).to include('has already been taken')
   end
+  
 end
