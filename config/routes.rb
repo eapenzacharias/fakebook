cacc5f0cf9d936f2816cc7dd1ab2332d3f86d6d3
lilleonz <lilleonz@gmail.com>
2019-09-09 14:44:01 -0400

    linter issues is not picking up changes

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
     authenticated :user do
       root 'feed#index', as: :authenticated_root
     end
+
     unauthenticated do
       root 'users/sessions#new', as: :unauthenticated_root
     end
