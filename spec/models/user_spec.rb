684ac79478fd59e1ddf633545ea30d4cf8be5293
eapenzacharias <eapenzac@gmail.com>
2019-09-12 01:31:09 +0200

    User Tests updated with password test cases

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -38,5 +38,16 @@ RSpec.describe User, type: :model do
       user.valid?
       expect(user.errors[:email]).to include('has already been taken')
     end
+
+    it 'is invalid without a password' do
+      user = build(:user, password: nil)
+      expect(user).to_not be_valid
+    end
+
+    it 'is invalid with a short password' do
+      user = build(:user, password: 'short')
+      expect(user).to_not be_valid
+    end
+
   end
 end
