641ac638f64147378f466cb90b86cfc2fb53dbd8
eapenzacharias <eapenzac@gmail.com>
2019-09-12 01:48:53 +0200

    post tests created

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -5,15 +5,25 @@ require 'rspec/rails'
 require 'shoulda/matchers'
 
 RSpec.describe Post, type: :model do
-  describe 'User and timeline associations' do
-    # it { should belong_to(:user).class_name('User') }
-    # it { should belong_to(:postable) }
+  before(:all) do
+    @post = create(:post)
+  end
+  it "is valid with vaild attributes" do
+    expect(@post).to be_valid
   end
 
-  describe 'factories and instance method' do
-    it 'has a valid factory' do
-      user = create(:user)
-      expect(create(:post, user_id: user.id)).to be_valid
-    end
+  it "is invalid without content" do
+    post = build(:post, content: nil)
+    expect(post).to_not be_valid
+  end
+
+  it "is invalid with empty content" do
+    post = build(:post, content: '')
+    expect(post).to_not be_valid
+  end
+
+  it "is invalid without a user" do
+    post = build(:post, user_id: nil)
+    expect(post).to_not be_valid
   end
 end
