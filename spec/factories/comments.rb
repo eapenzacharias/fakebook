d23f4f934d8308acc5c4f2a0e3da9e0787bc3d41
eapenzacharias <eapenzac@gmail.com>
2019-09-24 02:04:41 +0200

    Comments Testing added

--- spec/factories/comments.rb
+++ spec/factories/comments.rb
@@ -1,7 +1,10 @@
+require 'factory_bot'
+
 FactoryBot.define do
+  email = Faker::Internet.email
   factory :comment do
-    body { "MyText" }
-    user { nil }
-    post { nil }
+    body { 'Some sample text as comment' }
+    association :user, factory: :user, email: email
+    association :post, factory: :post
   end
 end
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,25 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    @user = create(:user)
+    @post = create(:post)
+    @comment = @user.comments.create(body: 'Sample text as comment', post: @post)
+  end
+
+  it 'creates a valid comment' do
+    expect(@comment).to be_valid  
+  end
+
+  it 'counts number of comments for a post' do
+    comment = @user.comments.create(body: 'Sample text as comment', post: @post)
+    expect(@post.comments.count).to eql(2)
+  end
+
+  it 'deletes a comment' do
+    @comment.destroy
+    expect(@post.comments.count).to eql(0)
+  end
 end
