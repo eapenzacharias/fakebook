32d60cbf9eb45cf82cf1246f342081804ade405d
eapenzacharias <eapenzac@gmail.com>
2019-09-25 00:56:36 +0200

    Removing acts-as-votable gem

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,5 +14,4 @@ class User < ApplicationRecord
   has_many :posts, dependent: :destroy
   has_many :likes, dependent: :destroy
   has_many :comments, dependent: :destroy
-  acts_as_voter
 end
--- spec/factories/likes.rb
+++
@@ -1,6 +0,0 @@
-FactoryBot.define do
-  factory :like do
-    post { nil }
-    user { nil }
-  end
-end
