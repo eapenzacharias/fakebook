38febaa3ac5412ed38b53871bee0b53daa2dfd5d
lilleonz <lilleonz@gmail.com>
2019-10-15 13:39:05 -0400

    2more linting issues

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,9 +1,10 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
+include Warden::Test::Helpers
 ENV['RAILS_ENV'] ||= 'test'
 
 require File.expand_path('../config/environment', __dir__)
-include Warden::Test::Helpers
+
 # Prevent database truncation if the environment is production
 abort('The Rails environment is running in production mode!') if Rails.env.production?
 require 'rspec/rails'
