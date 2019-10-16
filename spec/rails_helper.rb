1fa26b33aba5bbba776daa3ad04afb25c26f760a
lilleonz <lilleonz@gmail.com>
2019-10-16 16:34:14 -0400

    fixed linting and rspec issue

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,4 +1,3 @@
-include Warden::Test::Helpers
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
@@ -61,4 +60,5 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+  config.include Warden::Test::Helpers
 end
