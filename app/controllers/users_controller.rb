5923064608dd985666d7dcb7801c1da875a7088e
eapenzacharias <eapenzac@gmail.com>
2019-10-18 23:22:00 +0200

    Lit error fix

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,8 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @recent_posts = @user.posts
-    @mutual_friendships = Friendship.where(user_id: current_user, confirmed: true, friend_id: Friendship.where(user_id: @user.id, confirmed: true).select('friend_id'))
+    @mutual_friendships = Friendship.where(user_id: current_user, confirmed: true,
+                                           friend_id: Friendship.where(user_id: @user.id, confirmed: true).select('friend_id'))
   end
 
   def self.all_except(user)
