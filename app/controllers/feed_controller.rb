ffafae252e30435b33d6aea8e55f4fc4ff205d02
lilleonz <lilleonz@gmail.com>
2019-10-23 13:22:28 -0400

    Revert "Merge remote-tracking branch 'origin/milestone-6-friendship-v2' into milestone-6-friendship-v2"
    This reverts commit 441672f3d654b87950998817b9297b61e11818c6.

--- app/controllers/feed_controller.rb
+++ app/controllers/feed_controller.rb
@@ -2,8 +2,7 @@
 
 class FeedController < ApplicationController
   def index
-    @recent_posts = Post.order(created_at: :desc).where(user_id: Friendship.where(user_id: current_user.id,
-                                                                                   confirmed:true).select('friend_id'))
+    @recent_posts = Post.order(created_at: :desc)
     @new_post = Post.new
   end
 end
