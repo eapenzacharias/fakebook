09877c69f4fbf99b7513d15e5bf8545fd052bc24
lilleonz <lilleonz@gmail.com>
2019-10-23 13:22:08 -0400

    adjusting friendship controller

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -12,12 +12,12 @@ class FriendshipsController < ApplicationController
   end
 
   def create
-    @friendship_request = current_user.friendships.build(friend_id: params[:friend_id], confirmed: false)
+    @friendship_request = Friendship.new(friend_id: params[:id], confirmed: false)
     @friendship_request.user_id = current_user.id
     user = User.find_by(id: params[:id])
     if @friendship_request.save
       flash[:success] = "You send a friend request to #{user}"
-    elsif current_user.friend?(user)
+    elsif current_user.friends?(user)
       flash[:alert] = "You and #{user} are already friends"
     else
       flash[:error] = "You already sent a friend request to  #{user}"
@@ -26,7 +26,7 @@ class FriendshipsController < ApplicationController
   end
 
   def update
-    f = Friendship.find_by(id: params[:id])
+    f = Friendship.find_by(friend_id: current_user.id, user_id: params[:id])
     user = User.find_by(id: f.user_id)
     if f.confirm_friendship
       flash[:success] = "Now you are a #{user}'s friend"
