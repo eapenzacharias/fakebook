894226c8ca602f83f6484293f9b01494f7f8efd4
eapenzacharias <eapenzac@gmail.com>
2019-10-23 18:43:37 +0200

    Feed only shows friends posts

--- app/controllers/feed_controller.rb
+++ app/controllers/feed_controller.rb
@@ -2,7 +2,8 @@
 
 class FeedController < ApplicationController
   def index
-    @recent_posts = Post.order(created_at: :desc)
+    @recent_posts = Post.order(created_at: :desc).where(user_id: Friendship.where(user_id: current_user.id,
+                                                                                   confirmed:true).select('friend_id'))
     @new_post = Post.new
   end
 end
