707dcb1fb23ccadda010091a9308637189602c88
eapenzacharias <eapenzac@gmail.com>
2019-10-24 23:42:43 +0200

    Fiexes in friendship controller

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -13,21 +13,19 @@ class FriendshipsController < ApplicationController
 
   def create
     @friendship_request = current_user.friendships.build(friend_id: params[:friend_id], confirmed: false)
-    #@friendship_request = Friendship.new(friend_id: params[:id], confirmed: false)
-    
-    user = User.find_by(id: params[:id])
+    user = User.find_by(id: params[:friend_id])
     if @friendship_request.save
-      flash[:success] = "You send a friend request to #{user}"
-    elsif current_user.friend?(user)
-      flash[:alert] = "You and #{user} are already friends"
+      flash[:success] = "You send a friend request to #{user.name}"
+    elsif current_user.friends.include?(user)
+      flash[:alert] = "You and #{user.name} are already friends"
     else
-      flash[:error] = "You already sent a friend request to  #{user}"
+      flash[:error] = "You already sent a friend request to  #{user.name}"
     end
     redirect_to friendships_path
   end
 
   def update
-    f = Friendship.find_by(friend_id: current_user.id, user_id: params[:id])
+    f = Friendship.find_by(id: params[:id])
     user = User.find_by(id: f.user_id)
     if f.confirm_friendship
       flash[:success] = "Now you are a #{user}'s friend"
