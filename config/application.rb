41e27cde806ac9115b8c0b4e1dba6f2d03cc3110
lilleonz <lilleonz@gmail.com>
2019-11-06 17:29:19 -0500

    heroku updates

--- config/application.rb
+++ config/application.rb
@@ -14,7 +14,15 @@ module Fakebook
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
-
+    config.secret_key_base = '077c995040d2d610fe9aa85e8b88db6372fc5164f7f1814948e8c836dba4cece3567172a6b61d25926e5d996bdd52cdaf4708f1ba6433be16f3cc31b9f2b6474'
+    
+    # Load ENV variables from credentials.yml file
+    config.before_configuration do
+      env_file = File.join(Rails.root, 'config', 'credentials.yml')
+      YAML.load(File.open(env_file))[Rails.env].each do |key, value|
+        ENV[key.to_s] = value
+      end if File.exists?(env_file)
+    end
     # config.factory_bot.definition_file_paths = ["spec/factories"]
     config.assets.paths << "#{Rails}/vendor/assets/fonts"
 
