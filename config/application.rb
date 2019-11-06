2492ed3722396a709999df913a9406c5ec0b24eb
lilleonz <lilleonz@gmail.com>
2019-11-06 17:34:59 -0500

    more heroku updates

--- config/application.rb
+++ config/application.rb
@@ -18,8 +18,8 @@ module Fakebook
     
     # Load ENV variables from credentials.yml file
     config.before_configuration do
-      env_file = File.join(Rails.root, 'config', 'credentials.yml')
-      YAML.load(File.open(env_file))[Rails.env].each do |key, value|
+      env_file = File.join(Rails.root, 'config', 'local_env.yml')
+      YAML.load(File.open(env_file)).each do |key, value|
         ENV[key.to_s] = value
       end if File.exists?(env_file)
     end
